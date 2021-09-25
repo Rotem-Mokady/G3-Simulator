7683b35e7a3b088a92b3990b72bc5e7dfbf00ac5
Rotem-Mokady <rotemmokady@gmail.com>
2021-09-25 16:37:26 +0300

    drop secrets file

--- configs/dash/settings.py
+++ configs/dash/settings.py
@@ -1,15 +1,11 @@
-import socket
-from configs.secrets import DEV_HOSTS_NAMES
-
-
 HOST = "0.0.0.0"
-PORT = 80
+PORT = 5000
 THREADED = True
 
 PROJECT_NAME = "G3-Simulator"
 PY_FILE_EXTENSION = ".py"
 
 TESTS_COMMAND_LINE = "python -m unittest"
-RUN_TESTS = socket.gethostname() in DEV_HOSTS_NAMES
+RUN_TESTS = True
 
 
